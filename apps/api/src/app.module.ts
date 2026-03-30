7ae88d32a7dc324caa0dea4c7b4bd225886e25cb
yabao <154517591+vngbh@users.noreply.github.com>
2026-03-31 02:49:22 +0900

    feat(api): add ConfigModule.forRoot to load .env into NestJS

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
@@ -9,6 +10,7 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
